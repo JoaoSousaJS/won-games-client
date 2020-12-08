3093ab8baa26943aea9e734cf8738a6003b01095
Joao <joao.rsousa23@gmail.com>
2020-12-08 16:06:36 +1030

    feat: add snapshop test

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -43,6 +43,7 @@ export const Wrapper = styled.button<WrapperProps>`
   ${({ theme, size, fullWidth, hasIcon }) => css`
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
+    cursor: pointer;
     border: 0;
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
