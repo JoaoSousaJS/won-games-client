fd584a9c4e664275ef5b088807be6e3f1bdd01d3
Joao <joao.rsousa23@gmail.com>
2020-12-08 23:32:13 +1030

    feat: add test to open and close mobile menu

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -36,3 +36,13 @@ export const MenuGroup = styled.div`
     }
   `}
 `
+
+type MenuFullProps = {
+  isOpen: boolean
+}
+
+export const MenuFull = styled.nav<MenuFullProps>`
+  ${({ isOpen }) => css`
+    opacity: ${isOpen ? 1 : 0};
+  `}
+`
