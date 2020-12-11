9d3e9554188b9b921ffdfd5052a85312f81c1781
Joao <joao.rsousa23@gmail.com>
2020-12-11 22:00:54 +1030

    feat: render button on highlight component

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -5,12 +5,14 @@ import Highlight from './index'
 
 const props = {
   title: 'Heading 1',
-  subtitle: 'Heading 2'
+  subtitle: 'Heading 2',
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2'
 }
 
 describe('<Highlight />', () => {
   it('should render the headings and buttons', () => {
-    const { container } = renderWithTheme(<Highlight {...props} />)
+    renderWithTheme(<Highlight {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /Heading 1/i })
@@ -19,8 +21,7 @@ describe('<Highlight />', () => {
     expect(
       screen.getByRole('heading', { name: /Heading 2/i })
     ).toBeInTheDocument()
-    expect(container.firstChild).toMatchSnapshot()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
   })
 })
