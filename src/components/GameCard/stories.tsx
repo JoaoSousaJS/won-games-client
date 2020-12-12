b89b5b1e90d97557f268be599062704d3f0868b3
Joao <joao.rsousa23@gmail.com>
2020-12-12 20:14:39 +1030

    feat: add GameCard component

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import GameCard, { GameCardProps } from '.'
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstar games',
+    img:
+      'https://steamcdn-a.akamaihd.net/steam/apps/1091500/header.jpg?t=1607696821',
+    price: 'AUD 235.00'
+  }
+} as Meta
+
+export const Basic: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
---
+++ src/components/GameCard/test.tsx
@@ -0,0 +1,35 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameCard from './index'
+
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar games',
+  img:
+    'https://steamcdn-a.akamaihd.net/steam/apps/1091500/header.jpg?t=1607696821',
+  price: 'AUD 235.00'
+}
+
+describe('<GameCard />', () => {
+  it('should render the game card', () => {
+    renderWithTheme(<GameCard {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /population zero/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /rockstar games/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('img', { name: /population zero/i })
+    ).toHaveAttribute(
+      'src',
+      'https://steamcdn-a.akamaihd.net/steam/apps/1091500/header.jpg?t=1607696821'
+    )
+
+    expect(screen.getByText(/AUD 235.00/i)).toBeInTheDocument()
+  })
+})
