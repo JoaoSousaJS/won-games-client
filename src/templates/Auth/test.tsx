c2d9a51bc34b2c4ec0b9c915f2acfc0273850350
Joao <joao.rsousa23@gmail.com>
2020-12-21 09:00:40 +1030

    feat: add test

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -11,13 +11,19 @@ describe('<Auth />', () => {
       </Auth>
     )
 
-    expect(screen.getAllByLabelText(/Won Games/i)[0]).toBeInTheDocument()
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
     expect(screen.getByText(/auth title/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i
+      })
+    )
     expect(
       screen.getByRole('heading', {
         name: /WON is the best and most complete gaming platform./i
       })
     ).toBeInTheDocument()
     expect(screen.getByText(/Won Games 2020/i)).toBeInTheDocument()
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
 })
