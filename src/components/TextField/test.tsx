012945950220d01041317a9947512fa88e2a331d
Joao <joao.rsousa23@gmail.com>
2020-12-20 12:21:00 +1030

    feat: add disabled option

--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -83,4 +83,46 @@ describe('<TextField />', () => {
     )
     expect(screen.getByTestId('icon').parentElement).toHaveStyle({ order: 1 })
   })
+
+  it('Does not changes its value when disabled', async () => {
+    const onInput = jest.fn()
+    renderWithTheme(
+      <TextField
+        onInput={onInput}
+        label="TextField"
+        labelFor="TextField"
+        id="TextField"
+        disabled
+      />
+    )
+
+    const input = screen.getByRole('textbox')
+    expect(input).toBeDisabled()
+    const text = 'This is my new text'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).not.toHaveValue(text)
+    })
+    expect(onInput).not.toHaveBeenCalledWith(text)
+  })
+
+  it('Is not accessible by tab when disabled', async () => {
+    const onInput = jest.fn()
+    renderWithTheme(
+      <TextField
+        onInput={onInput}
+        label="TextField"
+        labelFor="TextField"
+        id="TextField"
+        disabled
+      />
+    )
+
+    const input = screen.getByLabelText('TextField')
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+    expect(input).not.toHaveFocus()
+  })
 })
