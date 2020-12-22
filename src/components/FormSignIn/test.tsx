816aa04ba590f300f35c88d832399892f10a90a8
Joao <joao.rsousa23@gmail.com>
2020-12-22 18:05:28 +1030

    feat: add FormSignIn component

---
+++ src/components/FormSignIn/test.tsx
@@ -0,0 +1,34 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import FormSignIn from './index'
+
+describe('<FormSignIn />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('button', { name: /sign in now/i })
+    ).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/Email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
+  })
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(
+      screen.getByRole('link', { name: /forgot your password?/i })
+    ).toBeInTheDocument()
+  })
+
+  it('should render the text to sign up', () => {
+    renderWithTheme(<FormSignIn />)
+
+    expect(screen.getByText(/do not have an account?/i)).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign up/i })).toHaveAttribute(
+      'href',
+      '/sign-up'
+    )
+  })
+})
