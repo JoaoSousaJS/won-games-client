d823eb884382579312c40785e179ff82ccb37b51
Joao <joao.rsousa23@gmail.com>
2020-12-29 12:31:23 +1030

    feat: add control to stories

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,19 +1,25 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import GameDetails, { GameDetailProps } from '.'
+import mockGame from './mock'
 
 export default {
   title: 'Game/GameDetails',
   component: GameDetails,
-  args: {
-    platforms: ['windows', 'linux', 'mac'],
-    releaseDate: '2020-11-21T23:00:00',
-    developer: 'Different Tales'
-  },
+  args: mockGame,
   argTypes: {
+    releaseDate: {
+      control: 'date'
+    },
     platforms: {
       control: {
         type: 'inline-check',
         options: ['windows', 'linux', 'mac']
+      }
+    },
+    genres: {
+      control: {
+        type: 'inline-check',
+        options: ['Role-playing', 'Narrative', 'Platform']
       }
     }
   },
