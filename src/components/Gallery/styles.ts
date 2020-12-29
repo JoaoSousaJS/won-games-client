aecb75103b4c1994e68685fb154349ceb0f52f58
Joao <joao.rsousa23@gmail.com>
2020-12-29 20:10:52 +1030

    feat: add close option to modal

--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -52,8 +52,20 @@ const modalModifiers = {
 }
 
 export const Modal = styled.div<ModalProps>`
-  ${({ isOpen }) => css`
+  ${({ theme, isOpen }) => css`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: ${theme.layers.modal};
+    transition: opacity ${theme.transition.default};
     ${isOpen && modalModifiers.open()}
     ${!isOpen && modalModifiers.close()}
   `}
 `
+export const Close = styled.div``
