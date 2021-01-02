a98a49c0913089492b6e69972e44936c30e7c309
Joao Sousa <joao.rsousa23@gmail.com>
2021-01-02 13:57:31 +1030

    feat: method to render game info on game template

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -18,7 +18,8 @@ export async function getStaticProps() {
       gameInfo: {
         title: 'Cyberpunk 2077',
         price: '59.00',
-        description: ''
+        description:
+          'Cyberpunk 2077 is an open-world, action-adventure story set in night city, a megalopis obessed with power, glamour and body modification. You play as V, mercenary outlaw going after a one-of-a-kind implant that is the key to immortality'
       }
     }
   }
