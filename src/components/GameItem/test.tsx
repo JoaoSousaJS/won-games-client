1e386c608666fc89fc85645ccf0b4359f79d78fb
Joao Sousa <joao.rsousa23@gmail.com>
2021-01-03 15:28:21 +1030

    feat: add link to GameItem component

--- src/components/GameItem/test.tsx
+++ src/components/GameItem/test.tsx
@@ -21,4 +21,14 @@ describe('<GameItem />', () => {
     )
     //expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the item with download link', () => {
+    const downloadLink = 'https://link'
+
+    renderWithTheme(<GameItem {...props} downloadLink={downloadLink} />)
+
+    expect(
+      screen.getByRole('link', { name: `Get ${props.title} here` })
+    ).toHaveAttribute('href', downloadLink)
+  })
 })
