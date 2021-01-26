b5b5967b8acc807a1c3dedb7b4823c754b899914
Joao Sousa <joao.rsousa23@gmail.com>
2021-01-26 20:02:04 +1030

    feat: add FormProfile component

---
+++ src/components/FormProfile/index.tsx
@@ -0,0 +1,10 @@
+import Heading from 'components/Heading'
+import * as S from './styles'
+
+const FormProfile = () => (
+  <S.Wrapper>
+    <Heading>My profile</Heading>
+  </S.Wrapper>
+)
+
+export default FormProfile
