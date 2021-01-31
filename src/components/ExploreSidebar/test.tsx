8cb0426904efd4d0ae76d23791d78744861a160f
Joao Sousa <joao.rsousa23@gmail.com>
2021-01-31 13:08:35 +1030

    feat: add ExploreSideBar component

---
+++ src/components/ExploreSidebar/test.tsx
@@ -0,0 +1,12 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import ExploreSidebar from './index'
+
+describe('<ExploreSidebar />', () => {
+  it('should render headings', () => {
+    renderWithTheme(<ExploreSidebar />)
+
+    expect(screen.getByRole('heading', { name: /price/i })).toBeInTheDocument()
+  })
+})
