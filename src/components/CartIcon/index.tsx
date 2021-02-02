63576ceae4d225a1457a089f475b844355746e58
Joao Sousa <joao.rsousa23@gmail.com>
2021-02-02 23:32:55 +1030

    feat: add CartIcon component

---
+++ src/components/CartIcon/index.tsx
@@ -0,0 +1,10 @@
+import { ShoppingCart } from '@styled-icons/material-outlined'
+import * as S from './styles'
+
+const CartIcon = () => (
+  <S.Wrapper>
+    <ShoppingCart aria-label="shopping Cart" />
+  </S.Wrapper>
+)
+
+export default CartIcon
