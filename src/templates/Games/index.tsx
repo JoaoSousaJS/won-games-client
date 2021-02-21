a6ddedc9672dc86bf4229b953778537ee518841d
Joao Sousa <joao.rsousa23@gmail.com>
2021-02-21 15:56:45 +1030

    feat: add show more function to load more games

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -2,6 +2,7 @@ import { KeyboardArrowDown } from '@styled-icons/material-outlined'
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
 import GameCard, { GameCardProps } from 'components/GameCard'
 import { Grid } from 'components/Grid'
+import { useQueryGames } from 'graphql/queries/games'
 import Base from 'templates/Base'
 import * as S from './styles'
 
@@ -10,29 +11,48 @@ export type GamesTemplateProps = {
   filterItems: ItemProps[]
 }
 
-const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => {
+const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
+  const { data, loading, fetchMore } = useQueryGames({
+    variables: { limit: 15 }
+  })
   const handleFilter = () => {
     return
   }
 
   const handleShowMore = () => {
-    return
+    fetchMore({
+      variables: {
+        limit: 15,
+        start: data?.games.length
+      }
+    })
   }
   return (
     <Base>
       <S.Main>
         <ExploreSidebar items={filterItems} onFilter={handleFilter} />
-        <section>
-          <Grid>
-            {games.map((item) => (
-              <GameCard key={item.title} {...item} />
-            ))}
-          </Grid>
-          <S.ShowMore role="button" onClick={handleShowMore}>
-            <p>Show More</p>
-            <KeyboardArrowDown size={35} />
-          </S.ShowMore>
-        </section>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <section>
+            <Grid>
+              {data?.games.map((game) => (
+                <GameCard
+                  key={game.slug}
+                  title={game.name}
+                  slug={game.slug}
+                  developer={game.developers[0].name}
+                  img={`http://localhost:1337${game.cover!.url}`}
+                  price={game.price}
+                />
+              ))}
+            </Grid>
+            <S.ShowMore role="button" onClick={handleShowMore}>
+              <p>Show More</p>
+              <KeyboardArrowDown size={35} />
+            </S.ShowMore>
+          </section>
+        )}
       </S.Main>
     </Base>
   )
