566a767fab681db9549da545cab9cd2bd2425021
Joao Sousa <joao.rsousa23@gmail.com>
2021-02-22 00:26:48 +1030

    feat: add where var to game query

--- src/graphql/queries/games.ts
+++ src/graphql/queries/games.ts
@@ -4,8 +4,8 @@ import { HighlightFragment } from 'graphql/fragments/highlight'
 import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 
 export const QUERY_GAMES = gql`
-  query QueryGames($limit: Int!, $start: Int) {
-    games(limit: $limit, start: $start) {
+  query QueryGames($limit: Int!, $start: Int, $where: JSON, $sort: String) {
+    games(limit: $limit, start: $start, where: $where) {
       ...GameFragment
     }
   }
