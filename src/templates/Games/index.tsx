3e4b4dafd25d929308436a820d9eb503a67d48a3
Joao Sousa <joao.rsousa23@gmail.com>
2021-02-24 23:58:04 +1030

    feat: show empty component if there is not games to show

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,4 +1,5 @@
 import { KeyboardArrowDown } from '@styled-icons/material-outlined'
+import Empty from 'components/Empty'
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
 import GameCard from 'components/GameCard'
 import { Grid } from 'components/Grid'
@@ -53,22 +54,31 @@ const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
           <p>Loading...</p>
         ) : (
           <section>
-            <Grid>
-              {data?.games.map((game) => (
-                <GameCard
-                  key={game.slug}
-                  title={game.name}
-                  slug={game.slug}
-                  developer={game.developers[0].name}
-                  img={`http://localhost:1337${game.cover!.url}`}
-                  price={game.price}
-                />
-              ))}
-            </Grid>
-            <S.ShowMore role="button" onClick={handleShowMore}>
-              <p>Show More</p>
-              <KeyboardArrowDown size={35} />
-            </S.ShowMore>
+            {data?.games.length ? (
+              <>
+                <Grid>
+                  {data?.games.map((game) => (
+                    <GameCard
+                      key={game.slug}
+                      title={game.name}
+                      slug={game.slug}
+                      developer={game.developers[0].name}
+                      img={`http://localhost:1337${game.cover!.url}`}
+                      price={game.price}
+                    />
+                  ))}
+                </Grid>
+                <S.ShowMore role="button" onClick={handleShowMore}>
+                  <p>Show More</p>
+                  <KeyboardArrowDown size={35} />
+                </S.ShowMore>
+              </>
+            ) : (
+              <Empty
+                title=":("
+                description="We didn't find any games with this filter"
+              />
+            )}
           </section>
         )}
       </S.Main>
--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -89,4 +89,16 @@ describe('<Games />', () => {
       query: { platforms: ['windows', 'linux'], sort_by: 'low-to-high' }
     })
   })
+
+  it('should render empty when no games found', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[]} addTypename={false}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+
+    expect(
+      await screen.findByText(/we didn't find any games with this filter/i)
+    ).toBeInTheDocument()
+  })
 })
