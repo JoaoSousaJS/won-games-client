cef2cea63f18fe20a18c262c30672a0b25c9617b
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-02 00:13:22 +1030

    feat: change cart components to get values from cart context

--- src/components/CartDropdown/test.tsx
+++ src/components/CartDropdown/test.tsx
@@ -2,19 +2,25 @@ import CartDropdown from './index'
 
 import items from 'components/CartList/mock'
 import { render, screen } from 'utils/test-utils'
+import { CartContextDefaultValues } from 'hooks/use-cart'
 
 describe('<CartDropdown />', () => {
+  beforeEach(() => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items,
+      quantity: items.length,
+      total: 'R$ 300,00'
+    }
+    render(<CartDropdown />, { cartProviderProps })
+  })
   it('should render <CartIcon /> and its badge', () => {
-    render(<CartDropdown items={items} total={'$300.00'} />)
-
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
     expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
   })
 
   it('should render dropdown content with cart items and total', () => {
-    render(<CartDropdown items={items} total={'$300.00'} />)
-
-    expect(screen.getByText('$300.00')).toBeInTheDocument()
+    expect(screen.getByText('R$ 300,00')).toBeInTheDocument()
     expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
   })
 })
--- src/components/CartIcon/test.tsx
+++ src/components/CartIcon/test.tsx
@@ -1,3 +1,4 @@
+import { CartContextDefaultValues } from 'hooks/use-cart'
 import { render, screen } from 'utils/test-utils'
 
 import CartIcon from './index'
@@ -11,16 +12,11 @@ describe('<CartIcon />', () => {
   })
 
   it('should render with badge', () => {
-    render(<CartIcon quantity={3} />)
+    render(<CartIcon />, {
+      cartProviderProps: { ...CartContextDefaultValues, quantity: 3 }
+    })
 
     expect(screen.getByLabelText(/cart item/i)).toBeInTheDocument()
     expect(screen.getByText(/3/i)).toBeInTheDocument()
-  })
-
-  it('should render with badge only if has positive numbers', () => {
-    render(<CartIcon quantity={-1} />)
-
-    expect(screen.queryByLabelText(/cart item/i)).not.toBeInTheDocument()
-    expect(screen.queryByText(/-1/i)).not.toBeInTheDocument()
   })
 })
