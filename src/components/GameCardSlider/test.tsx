50f47f96532d32ccc25a3bf79909c6c934be913a
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-02 20:54:18 +1030

    feat: add id to mock

--- src/components/GameCardSlider/test.tsx
+++ src/components/GameCardSlider/test.tsx
@@ -3,56 +3,7 @@ import 'match-media-mock'
 import GameCardSlider from './index'
 import { render, screen } from 'utils/test-utils'
 
-const items = [
-  {
-    slug: 'population-zero',
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x140',
-    price: 235,
-    promotionalPrice: 215
-  },
-  {
-    slug: 'population-zero',
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x141',
-    price: 235,
-    promotionalPrice: 215
-  },
-  {
-    slug: 'population-zero',
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x142',
-    price: 235,
-    promotionalPrice: 215
-  },
-  {
-    slug: 'population-zero',
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x143',
-    price: 235,
-    promotionalPrice: 215
-  },
-  {
-    slug: 'population-zero',
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x144',
-    price: 235,
-    promotionalPrice: 215
-  },
-  {
-    slug: 'population-zero',
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://source.unsplash.com/user/willianjusten/300x145',
-    price: 235,
-    promotionalPrice: 215
-  }
-]
+import items from './mock'
 
 describe('<GameCardSlider />', () => {
   it('should render with 4 active items', () => {
