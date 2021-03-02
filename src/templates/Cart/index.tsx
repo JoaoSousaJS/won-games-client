a27a4f86712fbdda58851b69ab9b45abe16624d4
Joao Sousa <joao.rsousa23@gmail.com>
2021-03-02 20:59:30 +1030

    refactor: remove item and total from cart template

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -1,7 +1,6 @@
 import CartList, { CartListProps } from 'components/CartList'
 import { Container } from 'components/Container'
 import { Divider } from 'components/Divider'
-import Empty from 'components/Empty'
 import { GameCardProps } from 'components/GameCard'
 import Heading from 'components/Heading'
 import { HighlightProps } from 'components/Highlight'
@@ -21,8 +20,6 @@ const Cart = ({
   recommendedGames,
   recommendedHighlight,
   recommendedTitle,
-  items,
-  total,
   cards
 }: CartProps) => {
   const handlePayment = () => ({})
@@ -32,19 +29,11 @@ const Cart = ({
         <Heading lineLeft lineColor="secondary">
           My cart
         </Heading>
-        {items?.length ? (
-          <S.Content>
-            <CartList items={items} total={total} />
+        <S.Content>
+          <CartList />
 
-            <PaymentOptions cards={cards} handlePayment={handlePayment} />
-          </S.Content>
-        ) : (
-          <Empty
-            title="You cart is empty"
-            description="Go back to the store and explore great games and offers"
-            hasLink
-          />
-        )}
+          <PaymentOptions cards={cards} handlePayment={handlePayment} />
+        </S.Content>
 
         <Divider />
       </Container>
