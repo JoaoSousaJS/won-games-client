41b6249fb204a43eff6fa8f373041a021147e5d7
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-23 22:59:28 +0930

    feat: add authprovider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import NextNprogress from 'nextjs-progressbar'
+import { Provider as AuthProvider } from 'next-auth/client'
 import { ApolloProvider } from '@apollo/client'
 import { CartProvider } from 'hooks/use-cart'
 import { AppProps } from 'next/app'
@@ -11,28 +12,30 @@ import { useApollo } from 'utils/apollo'
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApoloState)
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <CartProvider>
-          <Head>
-            <title>Won Games</title>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch icon" href="/img/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-            <meta name="description" content="A good game store" />
-          </Head>
-          <GlobalStyles />
-          <NextNprogress
-            color="#F231A5"
-            startPosition={0.3}
-            stopDelayMs={200}
-            height={3}
-            showOnShallow={true}
-          />
-          <Component {...pageProps} />
-        </CartProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+    <AuthProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CartProvider>
+            <Head>
+              <title>Won Games</title>
+              <link rel="shortcut icon" href="/img/icon-512.png" />
+              <link rel="apple-touch icon" href="/img/icon-512.png" />
+              <link rel="manifest" href="/manifest.json" />
+              <meta name="description" content="A good game store" />
+            </Head>
+            <GlobalStyles />
+            <NextNprogress
+              color="#F231A5"
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={3}
+              showOnShallow={true}
+            />
+            <Component {...pageProps} />
+          </CartProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </AuthProvider>
   )
 }
 
