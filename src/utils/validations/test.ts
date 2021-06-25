731a2500f9f59dfd1f9e2b65ba98e66945aef1f9
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-25 23:00:26 +0930

    test: should validate empty fields

---
+++ src/utils/validations/test.ts
@@ -0,0 +1,34 @@
+import { signInValidate, signUpValidate } from '.'
+
+describe('validations', () => {
+  describe('signInValidate()', () => {
+    it('should validate empty fields', () => {
+      const values = { email: '', password: '' }
+
+      expect(signInValidate(values)).toMatchObject({
+        email: '"email" is not allowed to be empty',
+        password: '"password" is not allowed to be empty'
+      })
+    })
+
+    it('should return invalid email error', () => {
+      const values = { email: 'invalid-email', password: 'asdasdas' }
+      expect(signInValidate(values).email).toMatchInlineSnapshot(
+        `"\\"email\\" must be a valid email"`
+      )
+    })
+  })
+
+  describe('signUpValidate()', () => {
+    it('should validate empty fields', () => {
+      const values = { username: '', email: '', password: '' }
+
+      expect(signUpValidate(values)).toMatchObject({
+        email: expect.any(String),
+        username: expect.any(String),
+        password: expect.any(String),
+        confirm_password: expect.any(String)
+      })
+    })
+  })
+})
