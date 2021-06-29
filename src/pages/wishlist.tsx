107a0fdd3f0e51d2d51ae82ac9966c487fbc15e2
Joao Sousa <joao.rsousa23@gmail.com>
2021-06-30 00:28:25 +0930

    feat: protect wishlist

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -5,13 +5,17 @@ import { initializeApollo } from 'utils/apollo'
 import { QueryRecommended } from 'graphql/generated/QueryRecommended'
 import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
 import { gamesMapper, highlightMapper } from 'utils/mappers'
+import { GetServerSidePropsContext } from 'next'
+import protectedRoutes from 'utils/protected-routes'
 
 export default function WishlistPage(props: WishListTemplateProps) {
   return <Wishlist {...props} />
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   const apolloClient = initializeApollo()
+
+  const session = await protectedRoutes(context)
 
   const { data } = await apolloClient.query<QueryRecommended>({
     query: QUERY_RECOMMENDED
@@ -23,7 +27,8 @@ export async function getStaticProps() {
       recommendedGames: gamesMapper(data.recommended?.section?.games),
       recommendedHighlight: highlightMapper(
         data.recommended?.section?.highlight
-      )
+      ),
+      session
     }
   }
 }
